993d000e463f0c6e51cd291bbc907ac885df21dc
Macbaren <macbaren123@gmail.com>
2022-05-06 17:42:55 +0300

    feat: 5 buttons rows

--- src/index.js
+++ src/index.js
@@ -17,6 +17,12 @@ SECOND_ROW.className = 'buttons-row';
 const THIRD_ROW = document.createElement('div');
 THIRD_ROW.className = 'buttons-row';
 
+const FOURTH_ROW = document.createElement('div');
+FOURTH_ROW.className = 'buttons-row';
+
+const FIFTH_ROW = document.createElement('div');
+FIFTH_ROW.className = 'buttons-row';
+
 document.body.appendChild(MAIN);
 MAIN.appendChild(TITLE);
 MAIN.appendChild(textareaContent);
@@ -74,10 +80,40 @@ const THIRD_ROW_ARR = [
   'Enter',
 ];
 
+const FOURTH_ROW_ARR = [
+  'Shift',
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+  ',',
+  '.',
+  '/',
+  '&#129093',
+  'Shift`',
+];
+
+const FIFTH_ROW_ARR = [
+  'Ctrl',
+  'Win',
+  'Alt',
+  '&#160',
+  'Alt',
+  '&#129092',
+  '&#129095',
+  '&#129094',
+  'Ctrl',
+];
+
 const ALL_ROWS = [
   { row: FIRST_ROW, array: FIRST_ROW_ARR },
   { row: SECOND_ROW, array: SECOND_ROW_ARR },
   { row: THIRD_ROW, array: THIRD_ROW_ARR },
+  { row: FOURTH_ROW, array: FOURTH_ROW_ARR },
+  { row: FIFTH_ROW, array: FIFTH_ROW_ARR },
 ];
 
 // ======FILL FIRST ROW=====
@@ -85,13 +121,18 @@ const fillRow = (row, rowArr) => {
   rowArr.forEach((el) => {
     const BUTTON = document.createElement('button');
     BUTTON.className = 'button-main-text';
+    BUTTON.id = `button${el}`;
     switch (el) {
+      case '&#160':
+        BUTTON.classList.add('button-space');
+        break;
       case 'Backspase':
       case 'CapsLock':
       case 'Shift':
         BUTTON.classList.add('button-big');
         break;
       case 'Enter':
+      case 'Shift`':
         BUTTON.classList.add('button-middle');
         break;
       case 'Tab':
@@ -101,13 +142,17 @@ const fillRow = (row, rowArr) => {
       case 'Ctrl':
       case 'Win':
       case 'Alt':
+      case '&#129092':
+      case '&#129093':
+      case '&#129094':
+      case '&#129095':
         BUTTON.classList.add('button-dark');
         break;
       default:
         break;
     }
 
-    BUTTON.textContent = el;
+    BUTTON.innerHTML = el;
     row.insertAdjacentElement('beforeend', BUTTON);
   });
 };
@@ -127,19 +172,22 @@ BUTTONS.forEach((button) => {
       console.log('EVENT', event.target.innerHTML);
       if (event.target.innerHTML === 'Backspase') {
         textareaContent.value = textareaContent.value.slice(0, -1);
+      } else if (event.target.innerHTML === 'Enter') {
+        textareaContent.value = textareaContent.value + '\r\n';
       } else textareaContent.value += event.target.innerHTML;
     },
     false
   );
+  button.removeEventListener;
 });
 
 document.addEventListener(
-  'keyup',
+  'keydown',
   (event) => {
-    const name = event.key;
-    const code = event.code;
-    // Alert the key name and key code on keydown
-    console.log(`event.key ${name} \r\n event.code ${code}`);
+    const buttonId = `button${event.key}`;
+    const pressedButton = document.querySelector(`#${buttonId}`);
+    pressedButton.classList.add('active', 'after');
+    console.log(pressedButton);
   },
   false
 );
